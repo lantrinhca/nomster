1ba1c0f06c3bb96a013f30650566305b7c0a745d
lantrinhca <lantrinh.ca@gmail.com>
2015-07-15 19:32:08 +0000

    Allow destroying of places only by owner

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-	before_action :authenticate_user!, :only => [:new, :create, :edit, :update]
+	before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :destroy]
 
 	def index
 		@places = Place.all.paginate(:page => params[:page], :per_page => 5).order('created_at ASC')
@@ -20,7 +20,6 @@ class PlacesController < ApplicationController
 
   def edit
   	@place = Place.find(params[:id])
-
   	if @place.user != current_user
 	    return render :text => 'Not Allowed', :status => :forbidden
 		end
@@ -31,13 +30,17 @@ class PlacesController < ApplicationController
   	if @place.user != current_user
     	return render :text => 'Not Allowed', :status => :forbidden
   	end
-  	
+
   	@place.update_attributes(place_params)
   	redirect_to root_path
   end
 
   def destroy
   	@place = Place.find(params[:id])
+  	if @place.user != current_user
+    	return render :text => 'Not Allowed', :status => :forbidden
+  	end
+
   	@place.destroy
   	redirect_to root_path
   end
