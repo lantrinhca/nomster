27f1ed489e418e618ecf0788ea199f0b42100ceb
lantrinhca <lantrinh.ca@gmail.com>
2015-07-23 11:36:36 +0000

    fix responsive images

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,9 @@ class Comment < ActiveRecord::Base
 	belongs_to :place
 	after_create :send_comment_email
 
+	validates :message, :presence => true
+	validates :rating, :presence => true
+
 	RATINGS = {
 		'one star' => '1_star',
 		'two stars' => '2_stars',
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,6 @@
 class Photo < ActiveRecord::Base
 	# belongs_to :user
 	belongs_to :place
+	validates :picture, :presence => true
 	mount_uploader :picture, PictureUploader
 end
